c486e2f36ec56be54540fcc87d90a36aa653c53e
Roger Guedez <guedez66@gmail.com>
2024-12-12 02:52:06 -0400

    first commit

---
+++ src/canastafeliz/CanastaFeliz.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package canastafeliz;
+import static javax.swing.JOptionPane.showMessageDialog;
+import view.Screen;
+
+/**
+ *
+ * @author rogerguedez
+ */
+public class CanastaFeliz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Screen screen = new Screen();
+        screen.setVisible(true);
+        
+        screen.setLocationRelativeTo(null);
+        
+       showMessageDialog(null, "This is even shorter");
+    }
+    
+}
